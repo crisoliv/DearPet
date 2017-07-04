5f2e8abf7f8934334f2b0b4cf829b93dcf193bde
Jhonnybsg <jhonnybsg.dev@gmail.com>
2017-07-03 23:55:06 -0400

    Login request integrated with view

--- Assets/Scripts/LoginController.cs
+++ Assets/Scripts/LoginController.cs
@@ -1,12 +1,20 @@
 ﻿using System.Collections;
+using UnityEngine.Networking;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class LoginController : MonoBehaviour {
 
+
+	private static string URL = "https://dearpet.000webhostapp.com/login_service.php?";
+
+	public InputField username;
+	public InputField password; 
+
 	// Use this for initialization
 	void Start () {
-        Screen.fullScreen = false;
+        Screen.fullScreen = true;
     }
 
     private void LateUpdate()
@@ -19,7 +27,44 @@ public class LoginController : MonoBehaviour {
    
     public void BtEntrar()
     {
-        SceneManager.LoadScene("Home");
+
+
+		StartCoroutine(sendPlayer (username.text, password.text));
+
+//        SceneManager.LoadScene("Home");
 
     }
+
+
+	IEnumerator sendPlayer(string username, string password) {
+
+
+		string login_attributes = "username=" + username + "&" +
+			"password=" + password;
+
+		Debug.Log ("Parametros: " + login_attributes);
+
+		UnityWebRequest www = UnityWebRequest.Get(URL + login_attributes);
+
+		yield return www.Send();
+
+		if(www.isError) {
+			Debug.Log(www.error);
+		}
+		else {
+
+			//string json = System.Text.Encoding.ASCII.GetString(www.downloadHandler.data);
+			string json = www.downloadHandler.text;
+
+
+			Debug.Log (json);
+			Message m = JsonUtility.FromJson<Message>(json.Replace("'", "\""));
+
+			if (m.status.Equals("true")) {
+				SceneManager.LoadScene("Home");
+			}
+
+		}
+
+	}
 }
---
+++ Assets/Scripts/Models/Message.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Message{
+
+	public string message;
+	public string status;
+
+}
