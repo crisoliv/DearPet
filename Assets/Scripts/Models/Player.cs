5021be5ea9755526e99be0538c629e3396787a31
Jhonnybsg <jhonnybsg.dev@gmail.com>
2017-07-03 22:49:39 -0400

    Login service and Player model added

---
+++ Assets/Scripts/Models/Player.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player{
+
+	private int id;
+	private string name;
+	private string username;
+	private string password;
+	private string sex;
+	private string birthday;
+
+	public Player (int id, string name, string username, string password, string sex, string birthday)
+	{
+		this.id = id;
+		this.name = name;
+		this.username = username;
+		this.password = password;
+		this.sex = sex;
+		this.birthday = birthday;
+	}
+	public Player(){
+	}
+
+	public int Id {
+		get {
+			return this.id;
+		}
+		set {
+			id = value;
+		}
+	}
+	
+	public string Name {
+		get {
+			return this.name;
+		}
+		set {
+			name = value;
+		}
+	}
+
+	public string Username {
+		get {
+			return this.username;
+		}
+		set {
+			username = value;
+		}
+	}
+
+	public string Password {
+		get {
+			return this.password;
+		}
+		set {
+			password = value;
+		}
+	}
+
+	public string Sex {
+		get {
+			return this.sex;
+		}
+		set {
+			sex = value;
+		}
+	}
+
+	public string Birthday{
+		get {
+			return this.birthday;
+		}
+		set {
+			birthday = value;
+		}
+	}
+}
---
+++ Assets/Scripts/NetworkRequests/UserLogin.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class UserLogin :MonoBehaviour {
+
+	public static Player player; 
+
+	private static string URL = "https://dearpet.000webhostapp.com/login_service.php?";
+
+
+	// Use this for initialization
+	void Start () {
+
+		// passar os dados da view e passar como parametro aqui
+		StartCoroutine(sendPlayer("taka", "123"));
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator sendPlayer(string username, string password) {
+
+		string login_attributes = "username=" + username + "&" +
+			"password=" + password;
+
+		UnityWebRequest www = UnityWebRequest.Get(URL + login_attributes);
+
+		yield return www.Send();
+
+		if(www.isError) {
+			Debug.Log(www.error);
+		}
+		else {
+
+			//string json = System.Text.Encoding.ASCII.GetString(www.downloadHandler.data);
+			string json = www.downloadHandler.text;
+			Debug.Log (json);
+//			player = JsonUtility.FromJson<Player>(json.Replace("'", "\""));
+//
+//			Debug.Log (player.Id);
+//			Debug.Log (player.Username);
+
+		}
+
+	}
+
+}
