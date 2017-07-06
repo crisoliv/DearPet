6dd00f9c9e310249739690889daff720840a6717
TakaBro <lekt@icomp.ufam.edu.br>
2017-07-06 19:27:17 -0400

    Customization Scene Added
    Customization Scene Added

---
+++ Assets/Scripts/CustomizationControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CustomizationControl : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        Screen.fullScreen = true;
+    }
+
+    public void BtBack()
+    {
+        SceneManager.LoadScene("Play");
+
+    }
+
+    private void LateUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Scripts/PlayControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayControl : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        Screen.fullScreen = false;
+    }
+
+    public void BtCustomization()
+    {
+        SceneManager.LoadScene("Customization");
+
+    }
+
+    private void LateUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
