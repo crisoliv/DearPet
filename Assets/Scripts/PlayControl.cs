5ada52ebda14d42214fb6b2e255de817d010ece4
TakaBro <lekt@icomp.ufam.edu.br>
2017-07-08 15:52:03 -0400

    Merged Play Scene with Customization Scene
    - Added new function BtCustomization over old function.
    - Set Active and Inactive objects when new function is called.
    - Change Background colors when new function is called.

--- Assets/Scripts/PlayControl.cs
+++ Assets/Scripts/PlayControl.cs
@@ -5,16 +5,73 @@ using UnityEngine.SceneManagement;
 
 public class PlayControl : MonoBehaviour {
 
+    bool whichItem = true;
+    public Camera camera;
+
+    public GameObject titleDearPet;
+    public GameObject titleCustom;
+
+    public GameObject itemPlay;
+    public GameObject itemCustom;
+
+    public GameObject itemPlayBack;
+    public GameObject itemCustomBack;
+
+    public GameObject txtCustom;
+    public GameObject txtBack;
+
+    public GameObject bar;
+
+    public Color colorWhite = Color.white;
+    public Color colorBlue;
+
+
     // Use this for initialization
     void Start()
     {
         Screen.fullScreen = false;
+        //camera = GetComponent<Camera>();
     }
 
     public void BtCustomization()
     {
-        SceneManager.LoadScene("Customization");
+        /*SceneManager.LoadScene("Customization");*/
+        if (whichItem)
+        {
+            titleDearPet.SetActive(false);
+            titleCustom.SetActive(true);
 
+            itemCustom.SetActive(true);
+            itemCustomBack.SetActive(true);
+            txtCustom.SetActive(false);
+
+            itemPlay.SetActive(false);
+            itemPlayBack.SetActive(false);
+            txtBack.SetActive(true);
+
+            bar.SetActive(false);
+
+            camera.backgroundColor = colorWhite;
+
+            whichItem = false;
+        }else {
+            titleDearPet.SetActive(true);
+            titleCustom.SetActive(false);
+
+            itemPlay.SetActive(true);
+            itemPlayBack.SetActive(true);
+            txtCustom.SetActive(true);
+
+            itemCustom.SetActive(false);
+            itemCustomBack.SetActive(false);
+            txtBack.SetActive(false);
+
+            bar.SetActive(true);
+
+            camera.backgroundColor = colorBlue;
+
+            whichItem = true;
+        }        
     }
 
     private void LateUpdate()
