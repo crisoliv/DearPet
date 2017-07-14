0332d372475312e5797c285d31340c4fa21214f3
Ivo Stinghen <iasf@icomp.ufam.edu.br>
2017-07-13 22:16:20 -0400

    Bug Fix

--- Assets/Scripts/HomeController.cs
+++ Assets/Scripts/HomeController.cs
@@ -6,7 +6,7 @@ public class HomeController : MonoBehaviour {
 
     void Start()
     {
-        if (!PlayerPrefs.HasKey("FirstApple")) PlayerPrefs.DeleteKey("FirstApple");
+        if (PlayerPrefs.HasKey("FirstApple")) PlayerPrefs.DeleteKey("FirstApple");
 
         Screen.fullScreen = true;
     }
