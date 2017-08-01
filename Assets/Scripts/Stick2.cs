10e884f7f8625915116e89dfea68cd13a20a28f4
TakaBro <lekt@icomp.ufam.edu.br>
2017-08-01 15:22:12 -0400

    Clothes Fixed
    - 2 Clothes Prefab Position Adjusted.
    - Stick2 Script created and added to object (second clothes).

---
+++ Assets/Scripts/Stick2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stick2 : MonoBehaviour
+{
+
+    public GameObject character;
+
+    void LateUpdate()
+    {
+
+        Vector2 newPos = character.transform.position;
+
+        newPos.y -= 1F;
+        newPos.x -= 0.05F;
+
+        transform.position = newPos;
+
+
+    }
+}
